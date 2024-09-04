9551fa7935a2827977e289d3dab99f1329f2f9d3
Alex <alexxbybee@westernwyoming.edu>
2024-09-03 21:11:47 -0600

    Add files via upload

---
+++ project01-02.js
@@ -0,0 +1,15 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Alexandra Stringer
+    Date:   September 2nd, 2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+    let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
+
+    let service1Speed = "50 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
